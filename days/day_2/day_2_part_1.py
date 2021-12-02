bea561d965cae6ca1b27d4a88391dafc19a277ac
sharkbound <laptopblaster@gmail.com>
2021-12-01 23:13:37 -0600

    solved day 2, used match/case for it, cause why not :D

--- days/day_2/day_2_part_1.py
+++ days/day_2/day_2_part_1.py
@@ -1,19 +1,42 @@
+from collections import namedtuple
 
 from day import Day
-import re
-import numpy as np
-import utils
+
+Instruction = namedtuple('Instruction', 'command units')
 
 
 class Day2Part1(Day):
     day = 2
     part = 1
-    
+
     def get_sample_input(self):
-        return ''
-    
-    def parse_input(self):
-        return ''
-        
+        return ('forward 5\n'
+                'down 5\n'
+                'forward 8\n'
+                'up 3\n'
+                'down 8\n'
+                'forward 2')
+
+    def parse_input(self) -> list[Instruction]:
+        return [
+            Instruction(command=command, units=int(unit))
+            for command, unit in map(str.split, self.input_text_lines)
+        ]
+
     def solve(self):
         data = self.parse_input()
+        x = 0
+        y = 0
+
+        for instr in data:
+            match instr:
+                case Instruction('up', units):
+                    y -= units
+                case Instruction('down', units):
+                    y += units
+                case Instruction('forward', units):
+                    x += units
+                case Instruction('back', units):
+                    x -= units
+
+        print(f'day 2 part 1 answer: {x * y}')
--- days/day_2/day_2_part_2.py
+++ days/day_2/day_2_part_2.py
@@ -1,19 +1,45 @@
+from collections import namedtuple
 
 from day import Day
-import re
-import numpy as np
-import utils
+
+Instruction = namedtuple('Instruction', 'command units')
 
 
-class Day2Part2(Day):
+class Day2Part1(Day):
     day = 2
     part = 2
-    
+
     def get_sample_input(self):
-        return ''
-    
-    def parse_input(self):
-        return ''
-        
+        return ('forward 5\n'
+                'down 5\n'
+                'forward 8\n'
+                'up 3\n'
+                'down 8\n'
+                'forward 2')
+
+    def parse_input(self) -> list[Instruction]:
+        return [
+            Instruction(command=command, units=int(unit))
+            for command, unit in map(str.split, self.input_text_lines)
+        ]
+
     def solve(self):
         data = self.parse_input()
+        x = 0
+        y = 0
+        aim = 0
+
+        for instr in data:
+            match instr:
+                case Instruction('up', units):
+                    aim -= units
+                case Instruction('down', units):
+                    aim += units
+                case Instruction('forward', units):
+                    x += units
+                    y += aim * units
+                case Instruction('back', units):
+                    x -= units
+                    y -= aim * units
+
+        print(f'day 2 part 2 answer: {x * y}')

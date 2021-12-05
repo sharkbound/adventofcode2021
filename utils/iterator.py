134db8e379fc7af9eb67684c05f059a36d41e1ea
sharkbound <laptopblaster@gmail.com>
2021-12-05 13:10:21 -0600

    fixed end marker yielding inside of iter_with_terminator being indented into the while loop

--- utils/iterator.py
+++ utils/iterator.py
@@ -26,5 +26,5 @@ def iter_with_terminator(iterable, end_marker=ITER_END_MARKER, include_end_marke
         if predicate(transformed := transform(value)):
             yield transformed
 
-        if include_end_marker:
-            yield end_marker
+    if include_end_marker:
+        yield end_marker

84f87bff5b41c0b3a6064758eb2808e01955fbae
sharkbound <laptopblaster@gmail.com>
2021-12-06 14:11:17 -0600

    solved day 6, this day was quite easy to solve surprisingly! very satisfying!

--- days/day_6/day_6_part_2.py
+++ days/day_6/day_6_part_2.py
@@ -17,7 +17,7 @@ class Day6Part2(Day):
         return '3,4,3,1,2'
 
     def parse_input(self):
-        return tuple(map(int, self.input_sample.split(',')))
+        return tuple(map(int, self.input_text.split(',')))
 
     def preload_counts(self, data):
         counts = deque([0] * 9)
@@ -37,22 +37,3 @@ class Day6Part2(Day):
         for _ in range(256):
             self.advance_phases(counts)
         self.print_answer(sum(counts))
-
-
-"""
-So, suppose you have a lanternfish with an internal timer value of 3:
-
-day:
-    1) its internal timer would become 2.
-    2) its internal timer would become 1.
-    3) its internal timer would become 0.
-    4) its internal timer would reset to 6, and it would create a new lanternfish with an internal timer of 8.
-    5) the first lanternfish would have an internal timer of 5, and the second lanternfish would have an internal timer of 7.
-
-A lanternfish that creates a new fish resets its timer to 6, not 7 
-(because 0 is included as a valid timer value). 
-The new lanternfish starts with an internal timer of 8 and does not start counting down until the next day.
-
-Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, 
-while each other number decreases by 1 if it was present at the start of the day.
-"""
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from day import run_day
 
 run_day(6, 1, timed=True)
-# run_day(6, 2, timed=True)
+run_day(6, 2, timed=True)

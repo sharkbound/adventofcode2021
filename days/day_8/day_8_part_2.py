28ca02de334505803f1bf0210cdd81e8f825510a
sharkbound <laptopblaster@gmail.com>
2021-12-11 17:31:12 -0600

    day 8 incorrectly corrects connections

--- days/day_8/day_8_part_2.py
+++ days/day_8/day_8_part_2.py
@@ -13,6 +13,27 @@ class Day8Part2(Day):
     day = 8
     part = 2
 
+    UNIQUE_LENGTH_TO_DIGITS = {2: ['1'], 3: ['7'], 4: ['4'], 5: ['2', '3', '5'], 6: ['0', '6', '9'], 7: ['8']}
+    UNIQUE_LENGTH_CONNECTIONS = {
+        '7': ((0, 'a'), (1, 'c'), (2, 'f')),
+        '8': ((0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g')),
+        '4': ((0, 'b'), (1, 'c'), (2, 'd'), (3, 'f')),
+        '1': ((0, 'c'), (1, 'f')),
+    }
+
+    SET_TO_NUMBER = {
+        frozenset('cf'): '1',
+        frozenset('acdeg'): '2',
+        frozenset('acdfg'): '3',
+        frozenset('bcdf'): '4',
+        frozenset('abdfg'): '5',
+        frozenset('abdefg'): '6',
+        frozenset('acf'): '7',
+        frozenset('abcdefg'): '8',
+        frozenset('abcdfg'): '9',
+        frozenset('abcefg'): '0'
+    }
+
     def get_sample_input(self):
         return 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
         # return ('be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\n'
@@ -37,66 +58,39 @@ class Day8Part2(Day):
             for line in self.input_sample_lines
         ]
 
-    unique_length_to_digits = {2: ['1'], 3: ['7'], 4: ['4'], 5: ['2', '3', '5'], 6: ['0', '6', '9'], 7: ['8']}
-    connection_mapping = {
-        '7': ((0, 'a'), (1, 'c'), (2, 'f')),
-        '8': ((0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g')),
-        '4': ((0, 'b'), (1, 'c'), (2, 'd'), (3, 'f')),
-        '1': ((0, 'c'), (1, 'f')),
-    }
-
-    set_to_number_mapping = {
-        frozenset('cf'): '1',
-        frozenset('acdeg'): '2',
-        frozenset('acdfg'): '3',
-        frozenset('bcdf'): '4',
-        frozenset('abdfg'): '5',
-        frozenset('abdefg'): '6',
-        frozenset('acf'): '7',
-        frozenset('abcdefg'): '8',
-        frozenset('abcdfg'): '9',
-        frozenset('abcefg'): '0',
-    }
-
     def solve(self):
         data = self.parse_input()
         for item in data:
-            set_to_digit = {}
-            known_connection_corrections = {}
 
             # find all the corrected connections from the unique length outputs
+            known_connection_corrections = {}
             for signal in item.signal_strs:
-                if (match := self.unique_length_to_digits.get(len(signal))) and len(match) == 1 and (match := match[0]):
-                    for index, correct_connection in self.connection_mapping[match]:
+                if (match := self.UNIQUE_LENGTH_TO_DIGITS.get(len(signal))) and len(match) == 1:
+                    for index, correct_connection in self.UNIQUE_LENGTH_CONNECTIONS[match[0]]:
                         # copy the corrected connection to the dictionary
+                        print(f'set {signal[index]} to {correct_connection}')
                         known_connection_corrections[signal[index]] = correct_connection
 
             _ = tuple(
-                self.set_to_number_mapping[corrected]
+                self.SET_TO_NUMBER[corrected]
                 for digit in item.digit_strs
-                if (corrected := ic(frozenset(map(known_connection_corrections.__getitem__, digit))))
+                if (corrected := ic(frozenset(map(known_connection_corrections.__getitem__, ic(digit)))))
             )
             ic(_)
 
 
 """
 acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
-                                                        
-         7=dab                                   
-         1=ab                                    
+
+correct mapping for the above line                                           
+               dddd                       ddd        aaa                                 
+              e    a                     |   a      b   c                                
+              e    a                     |   a      b   c                                
+               ffff                        -         ddd                                 
+              g    b                     |   b      e   f                                
+              g    b                     |   b      e   f                                                                     
+               cccc                       ---        ggg                                               
                                                  
-                                                 
-             ddd        aaa                             
-            |   a      b   c                            
-            |   a      b   c                            
-              -         ddd                        
-            |   b      e   f                            
-            |   b      e   f                            
-             ---        ggg                             
-                                                        
-                                                        
-                                                        
-                                                        
 
 *** Each entry consists of ten unique signal patterns, a | delimiter, and finally the four digit output value. ***
 

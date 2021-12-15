a8663a533e0134bd96a2efc8a986ad323bfa3b15
sharkbound <laptopblaster@gmail.com>
2021-12-14 22:33:44 -0600

    getting further on day 8:2

--- days/day_8/day_8_part_2.py
+++ days/day_8/day_8_part_2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict, Counter
+from dataclasses import dataclass
+
 from icecream import ic
 
 import utils
@@ -8,25 +11,25 @@ class Day8Part2(Day):
     day = 8
     part = 2
 
-    UNIQUE_LENGTH_CONNECTIONS = {
-        '7': ((0, 'a'), (1, 'c'), (2, 'f')),
-        '8': ((0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g')),
-        '4': ((0, 'b'), (1, 'c'), (2, 'd'), (3, 'f')),
-        '1': ((0, 'c'), (1, 'f')),
-    }
+    # UNIQUE_LENGTH_CONNECTIONS = {
+    #     '7': ((0, 'a'), (1, 'c'), (2, 'f')),
+    #     '8': ((0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g')),
+    #     '4': ((0, 'b'), (1, 'c'), (2, 'd'), (3, 'f')),
+    #     '1': ((0, 'c'), (1, 'f')),
+    # }
 
-    SET_TO_NUMBER = {
-        frozenset('cf'): '1',
-        frozenset('acdeg'): '2',
-        frozenset('acdfg'): '3',
-        frozenset('bcdf'): '4',
-        frozenset('abdfg'): '5',
-        frozenset('abdefg'): '6',
-        frozenset('acf'): '7',
-        frozenset('abcdefg'): '8',
-        frozenset('abcdfg'): '9',
-        frozenset('abcefg'): '0'
-    }
+    # SET_TO_NUMBER = {
+    #     frozenset('cf'): '1',
+    #     frozenset('acdeg'): '2',
+    #     frozenset('acdfg'): '3',
+    #     frozenset('bcdf'): '4',
+    #     frozenset('abdfg'): '5',
+    #     frozenset('abdefg'): '6',
+    #     frozenset('acf'): '7',
+    #     frozenset('abcdefg'): '8',
+    #     frozenset('abcdfg'): '9',
+    #     frozenset('abcefg'): '0'
+    # }
 
     def get_sample_input(self):
         return 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf'
@@ -52,26 +55,61 @@ class Day8Part2(Day):
             for line in self.input_sample_lines
         ]
 
+    def update_candidates(self, candidates, segments, data):
+        for segment in segments:
+            for value in data:
+                candidates[segment][value] += 1
+
     def solve(self):
         data = self.parse_input()
-        for item in data:
-            known_connection_corrections = {}
-            found = set()
+        for entry in data:
+            candidates = defaultdict(Counter)
+            candidates2 = defaultdict(Counter)
 
-            for signal in sorted(item.signal_strs, key=len):
+            for signal in entry.signal_strs:
+                # region match case
                 match len(signal):
-                    case 2:
-                        match = '1'
-                    case 3:
-                        match = '7'
-                    case 4:
-                        match = '4'
-                    case 7:
-                        match = '8'
+                    case 2:  # 1
+                        self.update_candidates(candidates2, 'cf', signal)
+                        candidates['c'][signal[0]] += 1
+                        candidates['f'][signal[1]] += 1
+                    case 3:  # 7
+                        self.update_candidates(candidates2, 'acf', signal)
+                        candidates['a'][signal[0]] += 1
+                        candidates['c'][signal[1]] += 1
+                        candidates['f'][signal[2]] += 1
+                    case 4:  # 4
+                        self.update_candidates(candidates2, 'bcdf', signal)
+                        candidates['b'][signal[0]] += 1
+                        candidates['c'][signal[1]] += 1
+                        candidates['d'][signal[2]] += 1
+                        candidates['f'][signal[3]] += 1
+                    case 7:  # 8
+                        self.update_candidates(candidates2, 'abcdefg', signal)
+                        candidates['a'][signal[0]] += 1
+                        candidates['b'][signal[1]] += 1
+                        candidates['c'][signal[2]] += 1
+                        candidates['d'][signal[3]] += 1
+                        candidates['e'][signal[4]] += 1
+                        candidates['f'][signal[5]] += 1
+                        candidates['g'][signal[6]] += 1
                     case _:
                         continue
+                # endregion
 
-            # rewriting to solved by lower length to higher length to attempt to solve collisions
+            ic([f'{k} -> {v.most_common(3)}' for k, v in candidates.items()])
+            ic([f'{k} -> {v.most_common(3)}' for k, v in candidates2.items()])
+
+            while any(len(c) for c in candidates.values()):
+                one_length_counters = [(key, counter) for key, counter in candidates.items() if len(counter) == 1]
+                ic(one_length_counters)
+                break
+
+            # atm this sorta works, it gets one right >>> ic| one_length_counters: [('e', Counter({'g': 1})), ('g', Counter({'b': 1}))]
+            # right one being e -> g
+            #
+            # thinking i need to rework it further to keep track of known ones based on len() sorting, eg: 1 is 2 length, so "ab" is should always be "cf"
+            # so i can remove "ab" from the mappings to check in the signals, and maybe mark "cf" as found, need to try it to see if it actually works or not...
 
 
 """

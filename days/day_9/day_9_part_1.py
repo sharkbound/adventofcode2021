71cf8605ce04ba790ae72b8c786157c6a43237c3
sharkbound <laptopblaster@gmail.com>
2022-01-22 17:45:04 -0600

    day 9 part 1 done

--- days/day_9/day_9_part_1.py
+++ days/day_9/day_9_part_1.py
@@ -1,4 +1,3 @@
-
 from day import Day
 import re
 import numpy as np
@@ -8,12 +7,30 @@ import utils
 class Day9Part1(Day):
     day = 9
     part = 1
-    
+
     def get_sample_input(self):
-        return ''
-    
+        return ('2199943210\n'
+                '3987894921\n'
+                '9856789892\n'
+                '8767896789\n'
+                '9899965678')
+
     def parse_input(self):
-        return ''
-        
+        return np.array(tuple(map(tuple, self.input_text_lines)), dtype='<u1')
+
+    def index_or_none(self, data, y, x):
+        if 0 <= y < data.shape[0] and 0 <= x < data.shape[1]:
+            return data[y, x]
+        return None
+
+    def is_low_point(self, data, y, x):
+        val_at_yx = data[y, x]
+        for offy, offx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            if (val := self.index_or_none(data, y + offy, x + offx)) is not None and val <= val_at_yx:
+                return False
+        return True
+
     def solve(self):
         data = self.parse_input()
+        Y_SIZE, X_SIZE = data.shape
+        self.print_answer(sum(val + 1 for (y, x), val in np.ndenumerate(data) if self.is_low_point(data, y, x)))
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from day import run_day
 
 TIMED = False
 
-# run_day(8, 1, timed=TIMED)
-run_day(8, 2, timed=TIMED)
+# run_day(9, 1, timed=TIMED)
+run_day(9, 2, timed=TIMED)

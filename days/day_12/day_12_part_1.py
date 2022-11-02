27fcee27fdb542b727068ab4015010bc0b2dd945
sharkbound <laptopblaster@gmail.com>
2022-11-01 22:11:09 -0500

    slight error fix, need to fix infinite path looping

--- days/day_12/day_12_part_1.py
+++ days/day_12/day_12_part_1.py
@@ -1,9 +1,19 @@
-from collections import defaultdict, deque
+from collections import defaultdict, deque, namedtuple
+from dataclasses import dataclass, field
+from typing import NamedTuple
 
+import numpy as np
 from icecream import ic
 
 import utils
 from day import Day
+
+
+@dataclass
+class Node:
+    name: str
+    children: set = field(default_factory=set)
+    parents: set = field(default_factory=set)
 
 
 class Day12Part1(Day):
@@ -18,6 +28,7 @@ class Day12Part1(Day):
                 'b-d\n'
                 'A-end\n'
                 'b-end')  # 10 paths
+
         # return ('dc-end\n'
         #         'HN-start\n'
         #         'start-kj\n'
@@ -30,24 +41,66 @@ class Day12Part1(Day):
         #         'kj-dc') # 19 paths
 
     def parse_input(self):
-        nodes = defaultdict(list)
-        for a, b in map(lambda x: x.split('-'), self.input_sample_lines):
-            nodes[a].append(b)
+        nodes = {}
+        for line in self.input_sample_lines:
+            parent, child = line.split('-')
+
+            if parent not in nodes:
+                nodes[parent] = Node(parent)
+
+            if child not in nodes:
+                nodes[child] = Node(child)
+
+            nodes[parent].name = parent
+            nodes[parent].children.add(child)
+
+            nodes[child].name = child
+            nodes[child].parents.add(parent)
+
         return nodes
 
-    def iter_paths_to_end(self, nodes):
-        remaining = deque([('start', nodes['start'], ())])
-        while remaining:
-            node, left, path = remaining.pop()
+    def iter_paths_to_end(self, src, nodes: defaultdict[str, Node]):
+        Info = namedtuple('Info', 'node seen path')
 
-            if node == 'end':
-                yield (*path, node)
+        remaining = deque([Info(node=nodes[src], seen=frozenset(), path=tuple())])
+        while remaining:
+            info = remaining.popleft()
+            if info.node.name == 'end':
+                yield info.path + (info.node.name,)
                 continue
 
-            for child in left:
-                remaining.append((child, nodes[child], (*path, node)))
+            if info.path and info.path[-1].isupper():
+                # todo: fix infinite loop
+                remaining.append(info._replace(node=nodes[info.path[-1]], seen=info.seen | {info.node.name}, path=info.path + (info.node.name,)))
+
+            if info.node.children:
+                for child in info.node.children:
+                    remaining.appendleft(info._replace(node=nodes[child], seen=info.seen | {info.node.name}, path=info.path + (info.node.name,)))
 
     def solve(self):
-        data = self.parse_input()
-        for path in self.iter_paths_to_end(data):
+        nodes = self.parse_input()
+        for path in self.iter_paths_to_end('start', nodes):
             print(path)
+
+
+"""
+
+    start
+    /   \
+c--A-----b--d
+    \   /
+     end
+
+start,A,b,A,c,A,end
+start,A,b,A,end
+start,A,b,end
+start,A,c,A,b,A,end
+start,A,c,A,b,end
+start,A,c,A,end
+start,A,end
+start,b,A,c,A,end
+start,b,A,end
+start,b,end
+
+
+"""
